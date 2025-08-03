de75f7e6026a0052a55c282ce6013075d4b2494c
Dzamir Akmal <Dzamir Akmal>
2025-08-03 11:20:56 +0700

    Tambah Seluruh File

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+import {Loading} from './components/Loading'
+import { useState } from "react";
+import { Navbar } from "./components/Navbar";
+import "./index.css"
+
+
+function App() {
+  const [isLoaded,setIsLoaded] = useState(false);
+  const [menuOpen,setMenuOpen] = useState(false);
+
+  return(
+  <>
+    {!isLoaded && <Loading onComplete={() => setIsLoaded(true)}/>}{""}
+      <div 
+      className={`min-h-screen transition-opacity duration-700 ${
+        isLoaded ? "opacity-100" : "opacity-0"
+      } bg-black text-gray-100`}
+      >
+        <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      </div>
+  </>
+  ) ;
+}
+
+export default App
