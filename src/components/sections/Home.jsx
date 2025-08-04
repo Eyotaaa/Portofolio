6b6411e0143aa8461de8870825d8770a02dd08ad
Dzamir Akmal <Dzamir Akmal>
2025-08-04 15:10:09 +0700

    update

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -4,9 +4,17 @@ export const Home = () => {
     className="min-h-screen flex items-center justify-center relative"
     >
         <div className="text-center z-10 px-4">
-            <h1 className="text--5xl md:text-7xl font-bold mb-6 gradient-text leading-right">
+            <h1 className="text--5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent leading-right">
                 Hi, I'm Dzamir Akmal
             </h1>
+            <p>
+                A Bachelor's graduate in Informatics Engineering from Telkom University, with a focus on Cybersecurity, 
+                Digital Forensics, Artificial Intelligence, Data Science, and Machine Learning. 
+                Actively developing skills in these fields with a strong enthusiasm for the latest technological advancements. 
+                Demonstrates good adaptability, along with effective communication and teamwork skills. 
+                Committed to continuous learning and contributing to tackling challenges in the world of 
+                cybersecurity and intelligent, data-driven technologies.
+            </p>
         </div>
     </section>
 }
