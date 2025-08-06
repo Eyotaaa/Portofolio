19613e0906608b4ee3f0298a23a85a8d57523ec1
Dzamir Akmal <Dzamir Akmal>
2025-08-06 15:05:02 +0700

    Home about update

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -34,13 +34,18 @@ export const About = () => {
                 About Me
             </h2>
 
-            <div classNamae="rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all">
+            <div classNamae="rounded-xl p-8 border-white/10 border-4 hover:-translate-y-1 transition-all">
                 <p className="text-gray-300 mb-6">
                     Actively developing skills in these fields with a strong
                     enthusiasm for the latest technological advancements
                 </p>
-                <div className="grid grid-cols-6 md:grid-cols-6 gap-9">
-                    <div className="col-start-1 col-end-3 rounded-xl p-6 hover:-translate-y-1 transition-all">
+
+                <div className="grid grid-cols-2 md:grid-cols-2 gap-9 ">
+
+                    <div className=" rounded-xl p-6 hover:-translate-y-1 transition-all rounded-xl border-1 border-orange-500/20">
+                        <div className="w-40 h-40 justify-center mx-auto">
+                            <img src="src\assets\Skill\forensic.png"></img>
+                        </div>
                         <h3 className="text-xl font-bold mb-4">
                             Digital Forensics
                         </h3>
@@ -55,7 +60,10 @@ export const About = () => {
                         </div>
                     </div>
 
-                    <div className="col-span-2 col-end-6 rounded-xl p-6 hover:-translate-y-1 transition-all">
+                    <div className="rounded-xl border-1 border-orange-500/20  rounded-xl p-6 hover:-translate-y-1 transition-all">
+                        <div className="w-40 h-40 justify-center mx-auto">
+                            <img src="src\assets\Skill\cyber.png"></img>
+                        </div>
                         <h3 className="text-xl font-bold mb-4">
                             Cyber Security
                         </h3>
@@ -71,7 +79,10 @@ export const About = () => {
                     </div>
 
 
-                    <div className="col-start-2 col-end-4 rounded-xl p-6 hover:-translate-y-1 transition-all">
+                    <div className=" rounded-xl border-1 border-orange-500/20 rounded-xl p-6 hover:-translate-y-1 transition-all">
+                        <div className="w-40 h-40 justify-center mx-auto">
+                            <img src="src\assets\Skill\data.png"></img>
+                        </div>
                         <h3 className="text-xl font-bold mb-4">
                             Data Science
                         </h3>
@@ -86,26 +97,15 @@ export const About = () => {
                         </div>
                     </div>
 
-                    <div className="col-start-1 rounded-xl p-6 hover:-translate-y-1 transition-all">
+                    <div className="rounded-xl border-1 border-orange-500/20 rounded-xl p-6 hover:-translate-y-1 transition-all">
+                        <div className="w-40 h-40 justify-center mx-auto">
+                            <img src="src\assets\Skill\ai.png"></img>
+                        </div>
                         <h3 className="text-xl font-bold mb-4">
                             Artificial Intelligence
                         </h3>
                         <div className="flex flex-wrap gap-2">
                             {artificialIntelligenceSkills.map((skill, key) => (
-                                <span key={key}
-                                    className="bg-yellow-500/10 text-yellow-500 px-3 py-1 rounded-full text-sm hover:bg-yellow-500/20 
-                                    hover:shadow-[0_0_15px_rgba(245, 158, 11,0.2)] transition">
-                                    {skill}
-                                </span>
-                            ))}
-                        </div>
-                    </div>
-                    <div className="col-start-3 rounded-xl p-6 hover:-translate-y-1 transition-all">
-                        <h3 className="text-xl font-bold mb-4">
-                            Frontend Development
-                        </h3>
-                        <div className="flex flex-wrap gap-2">
-                            {frontendSkills.map((skill, key) => (
                                 <span key={key}
                                     className="bg-yellow-500/10 text-yellow-500 px-3 py-1 rounded-full text-sm hover:bg-yellow-500/20 
                                     hover:shadow-[0_0_15px_rgba(245, 158, 11,0.2)] transition">
--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -1,28 +1,43 @@
 export const Home = () => {
-    return <section 
-    id="home"
-    className="min-h-screen flex items-center justify-center relative"
+    return <section
+        id="home"
+        className="min-h-screen flex items-center justify-center relative "
     >
-        <div className="text-center z-10 px-4">
-            <h1 className="text--5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-yellow-500 to-orange-400 bg-clip-text text-transparent leading-right">
-                Hi, I'm Dzamir Akmal
-            </h1>
-            <p className="text-gray-400 text-lg mb-8 max-w-lg mx-auto">
-                A Bachelor's graduate in Informatics Engineering from Telkom University,  
-                Committed to continuous learning and contributing to tackling challenges in the world of 
-                cybersecurity and intelligent, data-driven technologies.
-            </p>
-            <div className="flex justify-center space-x-4">
-                <a href="#projects"
-                className="bg-orange-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(251, 191, 36,0.4)]">
-                    View Projects
-                </a>
-                <a href="#Contact"
-                className="border border-orange-500/50 text-orange-500 py-3 px-6 rounded font-medium transition-all duration-200 hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(251, 191, 36,0.4)]
-                 hover:bg-blue-500/10">
-                    Contact Me
-                </a>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-9 items-center max-w-6xl px-4 mx-auto">
+
+            <div className=" flex items-center justify-center mt-10">
+                < img src="src\assets\DSC01517.JPG" className="rounded-full w-80 h-80 mx-auto mb-6 
+                object-cover shadow-lg border-4 border-orange-500 hover:scale-110 transition-transform duration-300 " >
+                </img>
             </div>
+            <div>
+                <h1 className="text--5xl md:text-5xl font-bold mb-4 bg-gradient-to-r 
+                from-yellow-500 to-orange-400 bg-clip-text text-transparent leading-right">
+                    Hi, I'm
+                </h1>
+                <span className="text--5xl md:text-5xl font-bold mb-4 bg-gradient-to-r 
+                from-yellow-500 to-orange-400 bg-clip-text text-transparent leading-right">
+                    Dzamir Akmal
+                </span>
+                <p className="text-gray-400 text-lg mb-4 max-w-lg mx-auto leading-right text-justify">
+                    A Bachelor's graduate in Informatics Engineering from Telkom University,
+                    Committed to continuous learning and contributing to tackling challenges in the world of
+                    cybersecurity and intelligent, data-driven technologies.
+                </p>
+                <div className="flex space-x-4">
+                    <a href="#projects"
+                        className="bg-orange-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(251, 191, 36,0.4)]">
+                        View Projects
+                    </a>
+                    <a href="#Contact"
+                        className="border border-orange-500/50 text-orange-500 py-3 px-6 rounded font-medium transition-all duration-200 hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(251, 191, 36,0.4)]
+                 hover:bg-blue-500/10">
+                        Contact Me
+                    </a>
+                </div>
+            </div>
+
         </div>
+
     </section>
 }
